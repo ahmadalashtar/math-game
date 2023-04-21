20a86583a66091cbb256e1d35420736f7a8f2b64
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-04-21 06:00:51 +0300

    Finalize bar position

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ window.onclick = function(){
 //                                      elemenets
 let buttons = document.getElementById('buttons');
 let game = document.getElementById('game');
-let start = document.getElementById('start')
+// let start = document.getElementById('start')
 let calculation = document.getElementById('calculation')
+let bar = document.getElementById('bar')
 
 //                                      center buttons vertically                
 let buttonsWidth = buttons.clientWidth
@@ -15,5 +16,28 @@ let halfbuttonswidthinpercent = (buttonsWidth*100/gameWidth)/2
 buttons.style.left = `${50-halfbuttonswidthinpercent}%`
 
 //                                      center start vertically                
-let startWidth = start.clientWidth
-start.style.left = `${50-(startWidth*100/gameWidth)/2}%`
+// let startWidth = start.clientWidth
+// start.style.left = `${50-(startWidth*100/gameWidth)/2}%`
+
+
+//                                      Bar Code
+const timeOut = new Event("timeOut");
+bar.addEventListener(
+    "timeOut",
+    (e) => {
+        
+    },
+    false
+  );
+const barFnc = function(){
+    bar.style.width = `${bar.clientWidth - 1}px`
+    if (bar.clientWidth==0) {
+         clearInterval(barInterval)
+         bar.dispatchEvent(timeOut);
+    }
+}
+const startBar = function(){
+const barInterval = setInterval(barFnc, 1);
+}
+
+
