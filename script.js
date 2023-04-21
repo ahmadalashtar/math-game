7d483afd70df9ebf0688047e5d69c1a9f73e6f32
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-04-21 03:33:45 +0300

    Center buttons with JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.onclick = function(){
    location.reload();
 }
+
+let buttons = document.getElementById('buttons');
+let game = document.getElementById('game');
+buttonsWidth = buttons.clientWidth
+gameWidth = game.clientWidth
+let halfbuttonswidthinpercent = (buttonsWidth*100/gameWidth)/2
+buttons.style.left = `${50-halfbuttonswidthinpercent}%`
