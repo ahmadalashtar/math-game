d3125755f3cf812fc8e62a30104b964498833f75
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-04-21 06:33:01 +0300

    Finalize responsive design

--- script.js
+++ script.js
@@ -2,25 +2,13 @@ window.onclick = function(){
    location.reload();
 }
 
-//                                      elemenets
+//                                                          elemenets
 let buttons = document.getElementById('buttons');
 let game = document.getElementById('game');
-// let start = document.getElementById('start')
 let calculation = document.getElementById('calculation')
 let bar = document.getElementById('bar')
 
-//                                      center buttons vertically                
-let buttonsWidth = buttons.clientWidth
-let gameWidth = game.clientWidth
-let halfbuttonswidthinpercent = (buttonsWidth*100/gameWidth)/2
-buttons.style.left = `${50-halfbuttonswidthinpercent}%`
-
-//                                      center start vertically                
-// let startWidth = start.clientWidth
-// start.style.left = `${50-(startWidth*100/gameWidth)/2}%`
-
-
-//                                      Bar Code
+//                                                           Bar Code
 const timeOut = new Event("timeOut");
 bar.addEventListener(
     "timeOut",
