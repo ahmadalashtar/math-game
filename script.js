19306d6a5ee7e30abe5bade1a5a0b27d1ae95f44
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-04-21 04:13:28 +0300

    is ready for game logic

--- script.js
+++ script.js
@@ -2,9 +2,24 @@ window.onclick = function(){
    location.reload();
 }
 
+//                                      elemenets
 let buttons = document.getElementById('buttons');
 let game = document.getElementById('game');
-buttonsWidth = buttons.clientWidth
-gameWidth = game.clientWidth
+let start = document.getElementById('start')
+
+//                                      center buttons vertically                
+let buttonsWidth = buttons.clientWidth
+let gameWidth = game.clientWidth
 let halfbuttonswidthinpercent = (buttonsWidth*100/gameWidth)/2
 buttons.style.left = `${50-halfbuttonswidthinpercent}%`
+
+//                                      center start vertically                
+let startWidth = start.clientWidth
+start.style.left = `${50-(startWidth*100/gameWidth)/2}%`
+
+for (let item of buttons.childNodes){
+    for (let i of item.childNodes){
+        console.log(i)
+    }
+}
+console.log(buttons.childNodes)
